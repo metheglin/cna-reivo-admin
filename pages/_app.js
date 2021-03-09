87cbaabcea91d3294b927eaad7c0962a060fab3b
metheglin <pigmybank@gmail.com>
2021-03-09 19:09:22 +0900

    Prepared spa-server.

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import {useEffect} from 'react'
 import FlashProvider from 'modules/rvadmin/core/FlashProvider'
 import SessionProvider from 'modules/rvadmin/core/SessionProvider'
 import {ThemeProvider} from '@themes/default'
@@ -6,7 +7,14 @@ import GlobalStyles from '../styles/GlobalStyles'
 import {QueryClient, QueryClientProvider} from 'react-query'
 const queryClient = new QueryClient()
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps, router}) {
+  useEffect(() => {
+    const initialAsPath = (typeof window === "undefined") ?
+      router.asPath :
+      `${window.location.pathname}${window.location.search}`
+    router.push(initialAsPath)
+  }, [])
+
   return (
     <ThemeProvider>
       <CssBaseline />
