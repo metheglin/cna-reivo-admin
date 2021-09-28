ff86ad227bfec82c8bdc45ecfae59a11f88e0f63
metheglin <pigmybank@gmail.com>
2021-09-28 21:11:06 +0900

    Updated deploy information.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
-module.exports = {
-  env: {
+const envMap = {
+  development: {
+    // Please use `.env.local` to set environment variables
   },
+  staging: {
+    NEXT_PUBLIC_API_PREFIX: "http://localhost:3000/admin_api/v1",
+    NEXT_PUBLIC_PAGE_ORIGIN: "http://localhost:3001",
+  },
+  production: {
+    NEXT_PUBLIC_API_PREFIX: "http://localhost:3000/admin_api/v1",
+    NEXT_PUBLIC_PAGE_ORIGIN: "http://localhost:3001",
+  },
+}
+
+function getEnv(envMap) {
+  if (process.env.NODE_ENV !== 'production') return envMap.development
+  return envMap[process.env.NEXT_PUBLIC_ENV] || envMap.development
+}
+
+const env = getEnv(envMap)
+const exportPathMap = (process.env.NODE_ENV !== 'production') ? null :
+  function () {
+    return {"/": { page: "/" }};
+  }
+
+module.exports = {
+  env: env,
+  exportPathMap: exportPathMap,
 }
