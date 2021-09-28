ba60b59a5d52ad092d2c3c77403da1cfa3d56999
metheglin <pigmybank@gmail.com>
2021-09-28 21:01:25 +0900

    Removed garbages.

--- components/residentapp/boards/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Form } from './Form'
-export { default as MainDashBar } from './MainDashBar'
-export { default as SubDashBar } from './SubDashBar'
-export { default as BuildingSummary } from './BuildingSummary'
